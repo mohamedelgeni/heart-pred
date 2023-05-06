7eba98b4c70197fe9cfd40ddeda1a9a07cbde56a
mohamedelgeni <36715062+mohamedelgeni@users.noreply.github.com>
2023-05-07 00:58:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,16 +16,16 @@ def main():
     # getting the input data from the user
     col1, col2, col3 = st.columns(3)
     with col1:
-         age = st.sidebar.slider("AGE", 21, 81, 23)
+         age = st.sidebar.selectbox("AGE", 21, 81, 23)
     with col2:
          sex = st.sidebar.radio("GENDER (1=Male, 0=Female)", ["1", "0"])
     with col3:   
         cp = st.sidebar.selectbox(
         "CHEST PAIN (0=Typical angina, 1=Atypical angina, 2=Non—anginal pain, 3=Asymptotic)", ["0", "1", "2", "3"])
     with col1:
-        trestbps = st.sidebar.slider("RESTING B.P.", 100, 400, 110)
+        trestbps = st.sidebar.selectbox("RESTING B.P.", 100, 400, 110)
     with col2:
-        chol = st.sidebar.slider("CHOLESTEROL", 100, 400, 110)
+        chol = st.sidebar.selectbox("CHOLESTEROL", 100, 400, 110)
     with col3:
         fbs = st.sidebar.radio(
         "FASTING BLOOD SUGAR (1=fbs>120mg/dl, 0=fbs<120 mg/dl)", ["1", "0"])
@@ -33,7 +33,7 @@ def main():
         restecg = st.sidebar.selectbox(
         "RESTING ECG (0=Normal ,1=Having ST-T wave abnormality, 2=Left ventricular hyperthrophy)", ["0", "1", "2"])
     with col2:
-        thalach = st.sidebar.slider("MAX. HEART BEAT ACHIEVED", 100, 200, 110)
+        thalach = st.sidebar.selectbox("MAX. HEART BEAT ACHIEVED", 100, 200, 110)
     with col3:
         exang = st.sidebar.radio(
         "EXERCISE INDUCED ANGINA(1=Yes, 0=No)", ["1", "0"])
@@ -44,7 +44,7 @@ def main():
         slope = st.sidebar.radio(
         "SLOPE OF THE PEAK EXERCISE ST SEGMENT (0=Upsloping, 1=Flatsloping, 2=Downsloping)", ["0", "1", "2"])
     with col3:
-        ca = st.sidebar.slider(
+        ca = st.sidebar.selectbox(
         "NUMBER OF VESSELS (0-3) COLORED BY FLUOROSCOPY", 0, 3, 1)
     with col2:
         thal = st.sidebar.radio(
