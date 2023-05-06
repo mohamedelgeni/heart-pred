ee8ec8df3beb2d0993f22ae67734b7d4855b5ea4
mohamedelgeni <36715062+mohamedelgeni@users.noreply.github.com>
2023-05-07 00:41:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,46 @@
 import streamlit as st
 import pickle
-import pandas as pd
-import numpy as np
-from PIL import Image
-import base64
 
-st.markdown("<h1 style='text-align: center; color:#42D1C6; font-size:50px;'>HEART DISEASE PREDICTION WEBAPP</h1>",unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; color:#HEX: #d5e1df; font-size:25px;'>This web app aims to help you find out whether you are at a risk of developing a heart disease or not.</p>", unsafe_allow_html=True)
+# Setting the page title and layout
+st.set_page_config(page_title='Heart Disease Prediction', layout='wide')
 
-heart_model = pickle.load(open('pipe.pkl','rb'))
+# Loading the trained model
+heart_model = pickle.load(open('pipe.pkl', 'rb'))
+
+# Page title and description
+st.markdown("<h1 style='text-align: center; color:#42D1C6; font-size:50px;'>HEART DISEASE PREDICTION WEBAPP</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color:#d5e1df; font-size:25px;'>This web app aims to help you find out whether you are at risk of developing a heart disease or not</p>", unsafe_allow_html=True)
 
 def main():
-    # getting the input data from the user
+    # Getting the input data from the user
+    st.write('### Input Parameters')
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.write("")
+        age = st.number_input("AGE", min_value=1, max_value=120, step=1, value=23)
+        trestbps = st.number_input("RESTING B.P.", min_value=50, max_value=250, step=1, value=110)
+        restecg = st.selectbox("RESTING ECG", ["Normal", "ST-T wave abnormality", "Left ventricular hypertrophy"])
+        oldpeak = st.number_input("ST DEPRESSION INDUCED", min_value=0.0, max_value=10.0, step=0.1, value=0.5)
     with col2:
-        st.write("")
-        age = st.sidebar.slider("AGE", 21, 81, 23)
-        sex = st.sidebar.radio("GENDER (1=Male, 0=Female)", ["1", "0"])
-        cp = st.sidebar.selectbox("CHEST PAIN (0=Typical angina, 1=Atypical angina, 2=Non—anginal pain, 3=Asymptotic)", ["0", "1", "2", "3"])
-        trestbps = st.sidebar.slider("RESTING B.P.", 100, 400, 110)
-        chol = st.sidebar.slider("CHOLESTEROL", 100, 400, 110)
-        fbs = st.sidebar.radio("FASTING BLOOD SUGAR (1=fbs>120mg/dl, 0=fbs<120 mg/dl)", ["1", "0"])
+        sex = st.radio("GENDER", ["Male", "Female"])
+        chol = st.number_input("CHOLESTEROL", min_value=50, max_value=600, step=1, value=200)
+        thalach = st.number_input("MAX. HEART BEAT ACHIEVED", min_value=50, max_value=250, step=1, value=150)
+        slope = st.selectbox("SLOPE OF THE PEAK EXERCISE ST SEGMENT", ["Upsloping", "Flatsloping", "Downsloping"])
     with col3:
-        st.write("")
-    with col1:
-        st.write("")
-        restecg = st.sidebar.selectbox("RESTING ECG (0=Normal ,1=Having ST-T wave abnormality, 2=Left ventricular hypertrophy)", ["0", "1", "2"])
-        thalach = st.sidebar.slider("MAX. HEART BEAT ACHIEVED", 100, 200, 110)
-        exang = st.sidebar.radio("EXERCISE INDUCED ANGINA(1=Yes, 0=No)", ["1", "0"])
-    with col2:
-        st.write("")
-        oldpeak = st.sidebar.slider("ST DEPRESSION INDUCED", 0.0, 5.0, 0.5)
-        slope = st.sidebar.radio("SLOPE OF THE PEAK EXERCISE ST SEGMENT (0=Upsloping, 1=Flatsloping, 2=Downsloping)", ["0", "1", "2"])
-    with col3:
-        st.write("")
-        ca = st.sidebar.slider("NUMBER OF VESSELS (0-3) COLORED BY FLUOROSCOPY", 0, 3, 1)
-        thal = st.sidebar.radio("THALASSEMIA (0 = normal; 1 = fixed defect; 2 = reversible defect)",["0", "1", "2"])
-
-    # Rest of your code...
-    # code for Prediction
-    diagnosis = ''
+        cp = st.selectbox("CHEST PAIN", ["Typical angina", "Atypical angina", "Non-anginal pain", "Asymptotic"])
+        fbs = st.selectbox("FASTING BLOOD SUGAR", ["<120 mg/dl", ">120 mg/dl"])
+        exang = st.selectbox("EXERCISE INDUCED ANGINA", ["No", "Yes"])
+        ca = st.number_input("NUMBER OF VESSELS COLORED BY FLUOROSCOPY", min_value=0, max_value=3, step=1, value=1)
+        
+    # Code for prediction
+    heart_pred = heart_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca]])
     
-    # creating a button for Prediction
-    heart_pred = heart_model.predict([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
-
-    if st.button("Predict"):    
+    if st.button("Predict"):
         if heart_pred[0] == 1:
-            st.error('Warning! You have high risk of getting a heart attack!')
+            st.error('Warning! You have a high risk of getting a heart attack!')
             st.write(f"Model Prediction: {heart_pred[0]} (1 = Heart disease present, 0 = No heart disease)")
         else:
-            st.success('You have lower risk of getting a heart disease!')
-            st.write(f"Model Prediction: {heart_pred[0]} (1 = Heart disease present, 0 = No heart disease)")
+                st.success('You have lower risk of getting a heart disease!')
+                st.write(f"Model Prediction: {heart_pred[0]} (1 = Heart disease present, 0 = No heart disease)")
           
-if __name__ == '__main__':
-    main()
-    
+    if __name__ == '__main__':
+        main()
